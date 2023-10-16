e18258284c3cc018dc509617ee5f4c72cac89fac
Alexander Naumkin <naumkin437@gmail.com>
2023-10-16 15:49:44 +0300

    added hw3

--- hw3/hw3.ts
+++ hw3/hw3.ts
@@ -1,32 +1,61 @@
-interface GeometricFigure {
+interface IGeometricFigure {
   readonly name: string;
   readonly color: string;
-  calculateArea(): number;
+  calculateArea(): void;
 }
 
-interface CalculationFormula {
+interface ICircle {
+  radius: number;
+}
+
+interface IRectangle {
+  height: number;
+  width: number;
+}
+
+interface ISquare {
+  height: number;
+  width: number;
+}
+
+interface ITriangle {
+  height: number;
+  width: number;
+}
+
+interface ICalculationFormula {
   print(): string;
 }
 
-class Circle implements GeometricFigure {
+abstract class Shape implements IGeometricFigure {
+  constructor(readonly name: string, readonly color: string) {}
+
+  calculateArea(): void {}
+}
+
+class Circle extends Shape implements ICircle {
   constructor(
     readonly name: string,
     readonly color: string,
     public radius: number
-  ) {}
+  ) {
+    super(name, color);
+  }
 
   calculateArea(): number {
     return Math.PI * (this.radius * this.radius);
   }
 }
 
-class Rectangle implements GeometricFigure, CalculationFormula {
+class Rectangle extends Shape implements IRectangle, ICalculationFormula {
   constructor(
     readonly name: string,
     readonly color: string,
     public height: number,
     public width: number
-  ) {}
+  ) {
+    super(name, color);
+  }
 
   calculateArea(): number {
     return this.height * this.width;
@@ -40,13 +69,15 @@ class Rectangle implements GeometricFigure, CalculationFormula {
   }
 }
 
-class Square implements GeometricFigure, CalculationFormula {
+class Square extends Shape implements ISquare, ICalculationFormula {
   constructor(
     readonly name: string,
     readonly color: string,
     public height: number,
     public width: number
-  ) {}
+  ) {
+    super(name, color);
+  }
 
   calculateArea(): number {
     return this.height * this.width;
@@ -60,59 +91,18 @@ class Square implements GeometricFigure, CalculationFormula {
   }
 }
 
-class Triangle implements GeometricFigure {
+class Triangle extends Shape implements ITriangle {
   constructor(
     readonly name: string,
     readonly color: string,
     public height: number,
     public width: number
-  ) {}
+  ) {
+    super(name, color);
+  }
 
   calculateArea(): number {
     return (this.height * this.width) / 2;
-  }
-}
-
-abstract class Shape {
-  constructor(protected name: string, protected color: string) {}
-
-  calculateArea(): void {}
-}
-
-class Trapezoid extends Shape implements CalculationFormula {
-  constructor(
-    name: string,
-    color: string,
-    protected height: number,
-    protected widthA: number,
-    protected widthB: number
-  ) {
-    super(name, color);
-    this.name = name;
-    this.color = color;
-    this.height = height;
-    this.widthA = widthA;
-    this.widthB = widthB;
-  }
-
-  get figureName(): string {
-    return this.name;
-  }
-
-  get figureColor(): string {
-    return this.color;
-  }
-
-  calculateArea(): number {
-    return (1 / 2) * (this.widthA + this.widthB) * this.height;
-  }
-
-  print(): string {
-    return `
-    S = 1/2 * (a + b) * h
-    h - height = ${this.height}
-    a - width a = ${this.widthA}
-    b - width b = ${this.widthB}`;
   }
 }
 
@@ -137,9 +127,3 @@ const triangle = new Triangle("Triangle", "green", 2, 2);
 console.log(`Figure name: ${triangle.name} `);
 console.log(`Figure color:  ${triangle.color}`);
 console.log(`Figure area = ${triangle.calculateArea()}`);
-
-const trapezoid = new Trapezoid("Trapezoid", "gray", 4, 3, 6);
-console.log(`Figure name: ${trapezoid.figureName}`);
-console.log(`Figure color:  ${trapezoid.figureColor}`);
-console.log(`Figure area = ${trapezoid.calculateArea()}`);
-console.log(`Formula for calculating area trapezoid: ${trapezoid.print()}`);
