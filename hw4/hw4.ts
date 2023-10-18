24c2740dc2212d6264033d6923e616ac30712898
Alexander Naumkin <naumkin437@gmail.com>
2023-10-18 16:54:28 +0300

    added hw4

---
+++ hw4/hw4.ts
@@ -0,0 +1,165 @@
+// ------------------------------------ Task №1
+
+interface IPerson {
+  [key: string]: number | string;
+}
+
+const person: IPerson = {
+  name: "John",
+  surname: "Lennon",
+  age: 30,
+};
+
+console.log(person);
+
+// ------------------------------------ Task №2
+
+type Types =
+  | string
+  | number
+  | boolean
+  | object
+  | any
+  | unknown
+  | null
+  | undefined;
+
+interface IGreet {
+  [key: string]: (arg: Types, arg2: Types) => void;
+}
+
+const sayHello: IGreet = {
+  greet: (fullName: string, age: number) => {
+    return `Hello, I'm ${fullName} and I'm ${age} years old`;
+  },
+};
+
+console.log(sayHello.greet("John Lennon", 30));
+
+// ------------------------------------ Task №3
+
+interface IUsers {
+  [key: number]:
+    | string
+    | {
+        name: string;
+        age: number;
+      }[];
+}
+
+const users: IUsers = {
+  0: "John",
+  1: "Jane",
+  2: "Bob",
+};
+
+const users2: IUsers = {
+  0: [
+    { name: "John", age: 25 },
+    { name: "Jane", age: 30 },
+    { name: "Bob", age: 35 },
+  ],
+  1: [
+    { name: "Bill", age: 25 },
+    { name: "Alex", age: 30 },
+    { name: "Kate", age: 35 },
+  ],
+  2: [
+    { name: "Polina", age: 25 },
+    { name: "Jane", age: 30 },
+    { name: "Stive", age: 35 },
+  ],
+};
+
+console.log(users[1]);
+console.log(users2[2]);
+
+// ------------------------------------ Task №4
+
+interface IUser {
+  [key: string]: Types;
+  name: string;
+}
+
+const john: IUser = {
+  name: "John",
+  age: 30,
+  email: "john@gmail.com",
+  man: true,
+  address: {
+    street: "123 Collins St",
+    city: "New York",
+  },
+};
+
+console.log(john);
+
+// ------------------------------------ Task №5
+
+interface IAnimals {
+  [key: string]: string | number;
+  name: string;
+  age: number;
+}
+
+interface IPets extends IAnimals {
+  [key: string]: Types;
+  voice: string;
+}
+
+const cat: IPets = {
+  name: "Kuzya",
+  age: 5,
+  voice: "meow",
+  vaccination: {
+    rabies: true,
+    leptospirosis: true,
+    piroplasmosis: false,
+  },
+};
+
+console.log(cat);
+
+// ------------------------------------ Task №6
+
+const bill: IUser = {
+  name: "Bill",
+  age: 30,
+  height: "181",
+  weight: 85,
+};
+
+const mark: IUser = {
+  name: "Mark",
+  age: 30,
+  height: 173,
+  weight: 81,
+};
+
+function validateUser(
+  user: IUser,
+  param: { [key: string]: (value: number) => boolean }
+): boolean {
+  for (const key in param) {
+    if (!param[key](user[key])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const isNumber = (value: number): boolean => {
+  return typeof value === "number";
+};
+
+const param = {
+  age: isNumber,
+  height: isNumber,
+  weight: isNumber,
+};
+
+const billIsValid = validateUser(bill, param);
+const markIsValid = validateUser(mark, param);
+
+console.log(billIsValid);
+console.log(markIsValid);
